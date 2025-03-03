473c09dba2cfcb44f0244927b5ebe6d1cc33e473
Antonio <antsaji2@gmail.com>
2025-03-03 12:36:10 +0100

    awiii

--- src/componentes/ProfileFoto.jsx
+++ src/componentes/ProfileFoto.jsx
@@ -2,7 +2,7 @@
 export default function ProfileFoto(){
     return(
         <div>
-        <img className="rounded-full max-h-96 shadow-[3px_3px_14px_4px_rgba(0,_0,_0,_0.2)] flex"   src="/public/fotoPerfil.jpg"/>
+        <img className="rounded-full max-h-96 shadow-[3px_3px_14px_4px_rgba(0,_0,_0,_0.2)] flex"   src="/fotoPerfil.jpg"/>
     </div>
     );
 }
